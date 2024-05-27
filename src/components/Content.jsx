2785704744f93178a8d7fff2cdd21ba4067b4f20
m4tiss <mateusz039@op.pl>
2024-05-27 11:49:08 +0200

    add axios

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import { React, useState, useEffect } from "react";
 import MatchList from './MatchList';
 import SideBar from './SideBar';
+import axios from "../config/axios";
 
 function Content() {
+
+  const [allmatches, setAllmatches] = useState([]);
+
+  useEffect(() => {
+    axios.get("/observator/matches/all").then((res) => {
+      const allmatches = res.data;
+      console.log(allmatches)
+      setAllmatches(allmatches);
+    });
+  }, []);
+
     return (
       <div className="flex flex-auto">
         <MatchList/>

210d72241817673a2314efe802ba7abdb0337773
m4tiss <mateusz039@op.pl>
2024-05-28 15:28:34 +0200

    change color in MatchPanel

--- src/components/MatchPanel.jsx
+++ src/components/MatchPanel.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { format } from 'date-fns';
+import { format } from "date-fns";
 
-
-function MatchPanel({match}) {
-
-    const formattedDate = format(new Date(match.match_date), "HH:mm dd-MM-yyyy");
+function MatchPanel({ match }) {
+  const formattedDate = format(new Date(match.match_date), "HH:mm dd-MM-yyyy");
 
   return (
     <div className="flex w-[600px] h-36 items-center justify-between bg-purple-600 rounded-mg border-b-4 border-t-4 border-yellow-400 px-5">
@@ -16,8 +14,18 @@ function MatchPanel({match}) {
         <div className="flex justify-center items-center">
           <h2 className="text-4xl">{match.result}</h2>
         </div>
-        <div className="bg-lime-400 text-white p-2 flex justify-center items-center">
-            {match.status}
+        <div
+          className={`${
+            match.status === "IN_PROGRESS"
+              ? "bg-orange-400 text-white"
+              : match.status === "FINISHED"
+              ? "bg-lime-400 text-black"
+              : match.status === "PLANNED"
+              ? "bg-pink-500 text-white"
+              : "bg-lime-400 text-black"
+          }  p-2 flex justify-center items-center`}
+        >
+          {match.status}
         </div>
       </div>
       <h2 className="text-white text-2xl">{match.name_b}</h2>

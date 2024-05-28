18b65987940507f00b8ab9448c8c95ccd6d88593
m4tiss <mateusz039@op.pl>
2024-05-28 17:14:07 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Content from "./components/Content";
 import NavBar from "./components/Navbar";
 import { MatchProvider } from "./providers/MatchProvider";
 import Login from "./pages/auth/Login";
-import { AuthProvider } from "./providers/AuthProvider";
+import { AuthProvider, useAuth } from "./providers/AuthProvider";
 
 function App() {
   return (
     <AuthProvider>
       <MatchProvider>
-        <div className="flex flex-col h-screen">
-          <NavBar />
-          <Login />
-          {/* <Content /> */}
-        </div>
+        <BrowserRouter>
+          <div className="flex flex-col h-screen">
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Navigate to="/login" />} />
+              <Route
+                path="login"
+                element={<AuthRoute component={<Login />} />}
+              />
+              <Route
+                path="content"
+                element={<PrivateRoute component={<Content />} />}
+              />
+            </Routes>
+          </div>
+        </BrowserRouter>
       </MatchProvider>
     </AuthProvider>
   );
+}
+
+function PrivateRoute({ component }) {
+  const { isAuth } = useAuth();
+
+  return isAuth() ? component : <Navigate to="/login" />;
+}
+
+function AuthRoute({ component }) {
+  const { isAuth } = useAuth();
+
+  return isAuth() ? <Navigate to="/content" /> : component;
 }
 
 export default App;

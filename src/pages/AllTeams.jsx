b406cef1455494ce10354c0003eb0d12a5738360
m4tiss <mateusz039@op.pl>
2024-05-28 17:55:27 +0200

    add teamPanel

--- src/pages/AllTeams.jsx
+++ src/pages/AllTeams.jsx
@@ -2,9 +2,15 @@ import {React,useState} from 'react';
 import axios from '../config/axios';
 
 function AllTeams() {
+
+
+
     return (
       <div className="flex w-full justify-center">
-            Wszystkie zespoły
+            <h2 className="text-3xl pt-8">Team list</h2>
+                {/* {allMatches.map((match) => ( */}
+                    {/* <TeamPanel key={match.id} match={match} /> */}
+                {/* ))} */}
       </div>
     );
   }

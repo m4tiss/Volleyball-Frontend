c5e433e6836af082f1c0607866bc6a04ec57550d
m4tiss <mateusz039@op.pl>
2024-05-29 20:50:41 +0200

    copy result finished match

--- src/pages/FinishedMatchDeatil.jsx
+++ src/pages/FinishedMatchDeatil.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { FaRegCopy } from "react-icons/fa";
 import axios from "../config/axios";
+import { format } from 'date-fns';
+
 
 function FinishedMatchDetail() {
   let { matchId } = useParams();
@@ -14,6 +17,7 @@ function FinishedMatchDetail() {
         const res = await axios.get(`/observator/matches/${matchId}`);
         const match = res.data;
         setMatch(match);
+        console.log(match)
         setRows([[match.name_a], [match.name_b]]);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -66,6 +70,96 @@ function FinishedMatchDetail() {
     return [[], []];
   };
 
+  const printSets = () => {
+
+  }
+
+  const printTeamA = () => {
+
+  }
+
+  const printTeamB = () => {
+
+  }
+
+  const formatMatchResult = (match) => {
+    let formattedResult = "";
+
+      const points_teama = [];
+      const points_teamb = [];
+      const numSets = match.result_detailed.resD.length;
+
+      let [sets_a, sets_b] = match.result.split(":").map(num => parseInt(num));
+
+      let longerName = match.name_a.length > match.name_b.length ? match.name_a : match.name_b;
+      let shorterName = match.name_a.length > match.name_b.length ? match.name_b : match.name_a;
+      let longerNameLength = longerName.length;
+      let shorterNameLength = shorterName.length;
+      let differnceSpace = longerNameLength - shorterNameLength
+        
+      match.result_detailed.resD.forEach((item) => {
+        const [pointsA, pointsB] = item.split(":").map(num => parseInt(num));
+        points_teama.push(pointsA);
+        points_teamb.push(pointsB);
+      });
+
+      formattedResult += ' '.repeat(longerNameLength + 1)
+
+
+      for (let i = 1; i <= numSets; i++) {
+        formattedResult += `S${i} | `;
+      }
+      formattedResult += `Total\n`;
+
+
+
+      if(shorterName === match.name_a ){
+        formattedResult += `${match.name_a} `;
+        formattedResult += ' '.repeat(differnceSpace)
+      }
+      else formattedResult += `${match.name_a} `
+
+
+      for (let i = 0; i < numSets; i++) {
+        formattedResult += `${points_teama[i]} | `;
+      }
+      formattedResult += `${sets_a}\n`;
+      
+      if(shorterName === match.name_b){
+        formattedResult += `${match.name_b} `;
+        formattedResult += ' '.repeat(differnceSpace)
+      }
+      else formattedResult += `${match.name_b} `;
+
+
+      for (let i = 0; i < numSets; i++) {
+        formattedResult += `${points_teamb[i]} | `;
+      }
+      formattedResult += `${sets_b}\n`;
+      
+
+      const formattedDate = format(new Date(match.match_date), "HH:mm:ss dd-MM-yyyy");
+
+      formattedResult += `${formattedDate}\n`;
+
+    return formattedResult;
+  };
+
+  const copyToClipboard = (formattedData) => {
+    navigator.clipboard.writeText(formattedData)
+      .then(() => {
+        console.log('Copied to clipboard');
+      })
+      .catch((error) => {
+        console.error('Error copying to clipboard:', error);
+      });
+  };
+
+  const handleCopyToClipboard = () => {
+    const formattedData = formatMatchResult(match);
+    copyToClipboard(formattedData);
+  };
+
   return (
     <div className="flex w-full items-center flex-col">
       <div className="flex my-8">
@@ -73,6 +167,10 @@ function FinishedMatchDetail() {
         <h2 className="text-4xl px-10">{match.result}</h2>
         <h2 className="text-4xl px-10">{match.name_b}</h2>
       </div>
+      <div onClick={handleCopyToClipboard} className="mb-10 cursor-pointer">
+      <FaRegCopy size={40}/>
+      </div>
+      
       {sets.map((set, index) => {
         const rowsForSet = setRowsForSet(index);
         return (

c2671239a1f3a203cfd6c56f4b103623f8c898e4
m4tiss <mateusz039@op.pl>
2024-05-29 14:27:13 +0200

    add team list

--- src/components/TeamPanel.jsx
+++ src/components/TeamPanel.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-function TeamPanel({ team }) {
+function TeamPanel({team}) {
   return (
-    <div className="">
-
+    <div className="w-60 text-3xl flex justify-center items-center p-5 rounded-xl shadow-xl
+     hover:scale-110 hover:duration-200 bg-yellow-300 m-5
+     cursor-pointer">
+      {team.name}
     </div>
   );
 }
--- src/pages/AllTeams.jsx
+++ src/pages/AllTeams.jsx
@@ -1,16 +1,38 @@
-import {React,useState} from 'react';
+import {React,useState,useEffect} from 'react';
+import { Navigate } from "react-router-dom";
 import axios from '../config/axios';
+import { useAuth } from '../providers/AuthProvider';
+import { useNavigate } from 'react-router-dom';
+import TeamPanel from '../components/TeamPanel';
 
 function AllTeams() {
 
+  const [allTeams, setAllTeams] = useState([]);
+  const navigate = useNavigate();
+
+  const {token} = useAuth
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/referee/teams`);
+        const teams = res.data;
+        setAllTeams(teams);
+      } catch (error) {
+        navigate('/content');
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData()
+  }, [token,navigate]);
 
 
     return (
-      <div className="flex w-full justify-center">
+      <div className="flex w-full items-center flex-col">
             <h2 className="text-3xl pt-8">Team list</h2>
-                {/* {allMatches.map((match) => ( */}
-                    {/* <TeamPanel key={match.id} match={match} /> */}
-                {/* ))} */}
+                {allTeams.map((team) => ( 
+                    <TeamPanel key={team.id} team={team} />
+                 ))}
       </div>
     );
   }

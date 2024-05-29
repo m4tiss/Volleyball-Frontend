b9948e4473b4f9526b0895cc25a244f24df2d5ea
m4tiss <mateusz039@op.pl>
2024-05-29 19:31:42 +0200

    get set,point,timeout from ws

--- src/pages/ActiveMatchDetails.jsx
+++ src/pages/ActiveMatchDetails.jsx
@@ -33,10 +33,21 @@ function ActiveMatchDetail() {
   }, [token]);
 
 
-  const extractNumbers = (score) => {
-    const numbers = score.split(":").map(num => parseInt(num));
+  const extractNumbers = (result) => {
+    const numbers = result.split(":").map(num => parseInt(num));
     return numbers;
   };
+
+  const extractPoints = (set) => {
+    const points = set.split(":").map(num => parseInt(num));
+    return points;
+  };
+
+  const extractTimeouts = (set_details) => {
+    const timeouts = set_details.split(":").map(num => parseInt(num));
+    return timeouts;
+  };
+
 
   return (
     <div className="flex w-full justify-center my-20">
@@ -46,17 +57,17 @@ function ActiveMatchDetail() {
           <ActiveTeamPanel 
           name={match.name_a}
           set={extractNumbers(match.result)[0]}
-          point={24}
+          point={extractPoints(match.result_detailed.resD[match.result_detailed.resD.length - 1])[0]}
           teamId={match.teama_id}
-          timeouts={2}
+          timeouts={extractTimeouts(match.result_detailed.timeout[match.result_detailed.timeout.length - 1])[0]}
           />
           <button className="bg-slate-300 h-fit text-white text-4xl pb-2 rounded-xl shadow-lg" onClick={handleSwap}>&#60;=&#62;</button>
           <ActiveTeamPanel
           name={match.name_b}
           set={extractNumbers(match.result)[1]}
-          point={21}
+          point={extractPoints(match.result_detailed.resD[match.result_detailed.resD.length - 1])[1]}
           teamId={match.teamb_id}
-          timeouts={1}
+          timeouts={extractTimeouts(match.result_detailed.timeout[match.result_detailed.timeout.length - 1])[1]}
           />
         </>
       ) : (
@@ -64,17 +75,17 @@ function ActiveMatchDetail() {
            <ActiveTeamPanel
            name={match.name_b}
            set={extractNumbers(match.result)[1]}
-           point={21}
+           point={extractPoints(match.result_detailed.resD[match.result_detailed.resD.length - 1])[1]}
            teamId={match.teamb_id}
-           timeouts={1}
+           timeouts={extractTimeouts(match.result_detailed.timeout[match.result_detailed.timeout.length - 1])[1]}
            />
           <button className="bg-slate-300 h-fit text-white text-4xl pb-2 rounded-xl shadow-lg" onClick={handleSwap}>&#60;=&#62;</button>
           <ActiveTeamPanel
            name={match.name_a}
            set={extractNumbers(match.result)[0]}
-           point={24}
+           point={extractPoints(match.result_detailed.resD[match.result_detailed.resD.length - 1])[0]}
            teamId={match.teama_id}
-           timeouts={2}
+           timeouts={extractTimeouts(match.result_detailed.timeout[match.result_detailed.timeout.length - 1])[0]}
            />
         </>
       ))}

9b6029ec3ed1a6e1c5050b7bb019812728a9fd49
m4tiss <mateusz039@op.pl>
2024-05-31 21:26:02 +0200

    change axois url

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: process.env.API_URL || "http://localhost:3000/",
+  baseURL: process.env.API_URL || "https://volleyball-backend.onrender.com/",
 });
 
 export default instance;

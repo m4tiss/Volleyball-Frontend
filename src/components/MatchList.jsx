74cb28c629677113d864600150eb0f951548bca8
m4tiss <mateusz039@op.pl>
2024-05-31 20:54:31 +0200

    add fetching matches in content

--- src/components/MatchList.jsx
+++ src/components/MatchList.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState ,useEffect } from "react";
 import MatchPanel from "./MatchPanel";
 import { useMatches } from "../providers/MatchProvider";
+import axios from "../config/axios";
 
 function MatchList() {
-  const { allMatches } = useMatches();
+
+  const[allMatches,setAllMatches] = useState([]);
+  const { status } = useMatches();
+
+    useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/observator/matches/all${status === 'ALL' ? '' : `/` + status}`);
+        const allMatchesData = res.data;
+        allMatchesData.sort((a, b) => new Date(b.match_date) - new Date(a.match_date));
+        setAllMatches(allMatchesData);
+        console.log(allMatchesData)
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, [status]);
+
 
   return (
     <div className="flex flex-col basis-3/4 items-center gap-10 mb-20">

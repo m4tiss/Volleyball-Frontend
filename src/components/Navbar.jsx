6001b836495a27437e1434908c0288d958ff294b
m4tiss <mateusz039@op.pl>
2024-06-01 16:06:59 +0200

    remove reload window

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,49 +6,28 @@ import axios from '../config/axios'
 
 function NavBar() {
 
-  const { isAuth,setToken,token } = useAuth()
+  const { isAuth,setToken,isReferee,user,setUser } = useAuth()
 
-  const [user, setUser] = useState();
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const res = await axios.get(`/auth/user`);
-        const user_data = res.data;
-        setUser(user_data);
-        console.log(user_data)
-      } catch (error) {
-        setUser(null);
-        console.error('Error fetching data:', error);
-      }
-    };
-    fetchUser()
-    if (isAuth) {
-      fetchUser();
-    } else {
-      setUser(null);
-    }
-  }, [isAuth,token]);
 
   const handleLogout = () =>{
     setToken(null)
     setUser(null)
-    window.location.reload();
   }
 
     return (
       <div className="flex bg-purple-700 py-5 text-white items-center justify-between">
         <div className="flex ml-10">
         <Link to="/content" className="text-3xl mx-10 cursor-pointer">VolleyWatch</Link>
-        {user && user.role === "referee" && <Link to="/allTeams" className="text-3xl mx-20 hover:text-yellow-400 border-none cursor-pointer">
+        {isReferee && <Link to="/allTeams" className="text-3xl mx-20 hover:text-yellow-400 border-none cursor-pointer">
             All teams
         </Link>}
 
-        {user && user.role === "referee" && <Link to="/addMatch" className="text-3xl mx-20 hover:text-yellow-400 border-none cursor-pointer">
+        {isReferee && <Link to="/addMatch" className="text-3xl mx-20 hover:text-yellow-400 border-none cursor-pointer">
             Add Match
         </Link>}
 
-        {user && user.role === "referee" && <Link to="/settings" className="text-3xl mx-20 hover:text-yellow-400 border-none cursor-pointer">
+        {isReferee && <Link to="/settings" className="text-3xl mx-20 hover:text-yellow-400 border-none cursor-pointer">
             Settings
         </Link>}
         
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -7,6 +7,8 @@ export const AuthProvider = ({ children }) => {
   const tokenKey = "authToken";
   const [token, setToken] = useState(localStorage.getItem(tokenKey) || null);
   const [finishHeader, setFinishHeader] = useState(false);
+  const [isReferee, setIsReferee] = useState(false);
+  const [user, setUser] = useState();
 
   useEffect(() => {
     if (token) {
@@ -19,12 +21,35 @@ export const AuthProvider = ({ children }) => {
     }
   }, [token]);
 
+  useEffect(() => {
+    const fetchUserRole = async () => {
+      if (finishHeader) {
+        try {
+          const res = await axios.get(`/auth/user`);
+          const user_data = res.data;
+          setUser(res.data)
+          if (user_data.role === "referee") {
+            setIsReferee(true);
+          } else {
+            setIsReferee(false);
+          }
+        } catch (error) {
+          setIsReferee(false);
+          setUser(null);
+          console.error("Error fetching data:", error);
+        }
+      }
+    };
+    fetchUserRole();
+  }, [token, finishHeader]);
+
   const isAuth = () => {
     return !!token;
   };
 
   return (
-    <AuthContext.Provider value={{ token, setToken, isAuth, finishHeader }}>
+    <AuthContext.Provider value={{ token, setToken, isAuth, finishHeader,
+     isReferee,user,setUser }}>
       {children}
     </AuthContext.Provider>
   );
